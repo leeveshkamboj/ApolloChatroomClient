379239d2ab00153a9d871bf920f079dd0893070d
Leevesh Kamboj <leeveshk12@gmail.com>
2021-07-16 11:49:07 +0530

    Secured

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 module.exports = {
   httpUrl: process.env.HTTP_URL || "https://apollochatroom.herokuapp.com/graphqll",
-  webSocketUrl: process.env.WEB_SOCKET_URL || "ws://apollochatroom.herokuapp.com/graphql",
+  webSocketUrl: process.env.WEB_SOCKET_URL || "wss://apollochatroom.herokuapp.com/graphql",
 };
